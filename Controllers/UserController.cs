f7eafb5a2bece0c7f876e23621eba21be779ee01
Miguel teheran <miguelteheran@gmail.com>
2020-09-05 18:33:34 -0500

    adding JWT

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using WebApiRoutesResponses.Services;
 
 namespace WebApiRoutesResponses.Controllers
 {
+    [EnableCors("MyPolicy")]
     [Route("api/[controller]")]
     [ApiController]
     public class UserController : ControllerBase
--- Startup.cs
+++ Startup.cs
@@ -13,6 +13,11 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using WebApiRoutesResponses.Services;
 using WebApiRoutesResponses.MiddleWares;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 namespace WebApiRoutesResponses
 {
@@ -28,8 +33,46 @@ namespace WebApiRoutesResponses
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers(config =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                                .RequireAuthenticatedUser()
+                                .Build();
+                config.Filters.Add(new AuthorizeFilter(policy));
+            });
+
             services.AddScoped<IUserDataService,UserDataService>();
+            services.AddCors(p=>
+            {
+                p.AddPolicy("MyPolicy",
+                builder=> {
+                     builder.AllowAnyHeader()
+                        .WithOrigins("http://127.0.0.1:5500")
+                        .WithMethods("GET", "POST", "PUT", "DELETE").
+                        Build();
+                });
+
+            });  
+
+            var key = Encoding.ASCII.GetBytes(Configuration.GetValue<string>("SecretKey"));
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options => {
+
+                    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                        ValidateLifetime =  true,
+                        ValidIssuer = "",
+                        ValidAudience = "",
+                        ValidateAudience = false,
+                        ValidateIssuer = false,
+                        ValidateIssuerSigningKey= true
+                    };
+            });          
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,9 +83,13 @@ namespace WebApiRoutesResponses
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
